ca7ac4bdb2e68a5b3f361ec75e5fb5e8c2325602
Loki Holbourn <lokibubbles@gmail.com>
2019-09-30 16:10:35 +0100

    Updates

---
+++ precache-manifest.16d6fe9b0991cdc5eea8b99989939de0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e104931bbc3306d483cf2c8b99f48f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec6a71956b9560d3429b",
+    "url": "/static/css/2.6b533975.chunk.css"
+  },
+  {
+    "revision": "a9ebbdac3cf1fde5f06e",
+    "url": "/static/css/main.27f7c511.chunk.css"
+  },
+  {
+    "revision": "ec6a71956b9560d3429b",
+    "url": "/static/js/2.cf62498a.chunk.js"
+  },
+  {
+    "revision": "a9ebbdac3cf1fde5f06e",
+    "url": "/static/js/main.33a3eb80.chunk.js"
+  },
+  {
+    "revision": "3c2210b84b35e0ca614f",
+    "url": "/static/js/runtime-main.7302273d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
