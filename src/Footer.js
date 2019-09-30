2b83b3527a614844309d83731019fdab260bada1
Loki Holbourn <lokibubbles@gmail.com>
2019-09-30 14:51:16 +0100

    Added footer with email address

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="row footer">
+      <div className="col-12 footer__content">
+        <h3 className="footer__content_normal">
+          Get in touch with me at
+          <span className="footer__content_bold"> loki@lokih.com</span>
+        </h3>
+      </div>
+    </div>
+  );
+}
