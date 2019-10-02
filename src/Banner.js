8a50251f95e3b886eb84f4d8d515932f7dff24ba
Loki Holbourn <lokibubbles@gmail.com>
2019-10-02 14:27:10 +0100

    Fixed responsive issues with banner

--- src/Banner.js
+++ src/Banner.js
@@ -5,14 +5,14 @@ import SocialLinks from "./SocialLinks";
 export default function Banner() {
   return (
     <div className="row banner">
-      <div className="col-sm-2 banner__avatar_wrapper">
+      <div className="col-md-3 col-lg-2 banner__avatar_wrapper">
         <img src="/loki.jpg" alt="Loki Holbourn" className="banner__avatar" />
       </div>
-      <div className="col-sm-9">
+      <div className="col-md-8 col-lg-9">
         <h1 className="banner__title">Loki Holbourn</h1>
         <h3 className="banner__subtitle">Full Stack Web Developer</h3>
       </div>
-      <div className="col-sm-1">
+      <div className="col-md-1">
         <SocialLinks />
       </div>
     </div>
