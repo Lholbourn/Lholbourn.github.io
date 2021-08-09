7712650affc54bd34df73698ac073cbe018f430d
Lholbourn <loki.holbourn@bbc.co.uk>
2021-08-09 10:42:11 +0100

    Removing email from footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,8 +6,8 @@ export default function Footer() {
     <div className="row footer">
       <div className="col-12 footer__content">
         <h3 className="footer__content_normal">
-          Get in touch with me at
-          <span className="footer__content_bold"> loki@lokih.com</span>
+          Get in touch with me on
+          <span className="footer__content_bold">LinkedIn</span>
         </h3>
       </div>
     </div>
